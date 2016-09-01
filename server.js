777e42e5f0d9b893daaafd4500baa1b75665edf0
Matúš Tomlein <matus.tomlein@gmail.com>
2016-09-01 15:22:36 +0200

    Minor changes

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 var http = require('http');
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -54,26 +47,11 @@ function getReadings(callback) {
   }).end();
 }
 
-var queryUrl = 'http://platform.sociotal.eu:3500/SocIoTal_CM_REST_V3/EXTENDED/queryContext/Superhik:CRS4:generic:Ethanol_sensor';
-
 app.get("/read", function (request, response) {
   getReadings(function (reading) {
     response.send(reading);
   });
 });
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-  ];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
